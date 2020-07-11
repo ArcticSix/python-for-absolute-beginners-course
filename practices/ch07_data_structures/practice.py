bb5eeb1c977c04765444df94e44f739da9641091
ArcticSix <68033641+ArcticSix@users.noreply.github.com>
2020-07-11 15:17:19 -0500

    Add files via upload
    My practice solution for Chapter 7

---
+++ practices/ch07_data_structures/practice.py
@@ -0,0 +1,17 @@
+
+# d = create d using core concepts above
+
+d = {
+    "Sam": 7,
+    'rolls': ['rock', 'paper', 'scissors'],
+    'done': True
+}
+
+
+
+
+print(d["Sam"])                 # outputs 7
+print(d['rolls'])               # outputs ['rock', 'paper', 'scissors']
+print(d.get('Sarah'))           # outputs None
+print(d.get('Jeff', -1))        # outputs -1
+print(d['done'])                # outputs True
